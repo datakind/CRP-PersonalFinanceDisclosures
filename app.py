449bda150ed70dc06e91580c05165908b6d0c064
Krishna Halaharvi <krishnahalaharvi@Krishnas-MacBook-Air.local>
2017-11-11 15:03:33 -0500

    adding algolia

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+import os
 from flask import Flask, render_template
+from algoliasearch import algoliasearch
+from dotenv import load_dotenv
+
 app = Flask(__name__)
+APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
+dotenv_path = os.path.join(APP_ROOT, '.env')
+load_dotenv(dotenv_path)
+
+algolia_api_key = os.getenv('ALGOLIA_API_KEY')
+algolia_app_key = os.getenv('ALGOLIA_APP_ID')
+client = algoliasearch.Client(algolia_app_key, algolia_api_key)
 
 @app.route('/')
 def index():
