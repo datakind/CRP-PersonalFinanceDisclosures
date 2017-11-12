027da8ce30cc85f21719fe94e1144745eb3a7835
Krishna Halaharvi <krishnahalaharvi@Krishnas-MacBook-Air.local>
2017-11-12 12:05:00 -0500

    adding index path

--- goal3/index.js
+++ goal3/index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = process.env.PORT  || 8080;
 require('dotenv').config()
 app.use(express.static('public'));
+const path = require('path');
 
+app.get('/', () => {
+  res.sendFile(`${path.resolve(__dirname, 'public')}/index.html`);
+})
 
 app.listen(port, () =>{
   console.log('app running on port ', port);
